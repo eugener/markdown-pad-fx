31fb29b010d0f933d8c1cd8ace6ca7d55e5455fb
Eugene Ryzhikov <eryzhikov@gmail.com>
2013-09-09 22:55:33 -0400

    Prepare more actions

--- markdown-pad-fx/src/main/java/com/ezware/markdownfx/Start.java
+++ markdown-pad-fx/src/main/java/com/ezware/markdownfx/Start.java
@@ -18,13 +18,13 @@ import javafx.stage.Stage;
 import org.controlsfx.control.action.AbstractAction;
 import org.controlsfx.control.action.ActionGroup;
 import org.controlsfx.control.action.ActionUtils;
+import org.controlsfx.control.action.ActionUtils.ActionTextBehavior;
 import org.controlsfx.dialog.Dialog;
 import org.controlsfx.dialog.Dialogs;
 
 public class Start extends javafx.application.Application implements DocumentEditorProvider {
 
     private TabPane tabs = new TabPane();
-    
 
     public static void main(String[] args) {
         launch(args);
@@ -35,22 +35,29 @@ public class Start extends javafx.application.Application implements DocumentEdi
         
         createDocumentEditor();
         
-        ActionNewEditor actionNewEditor = new ActionNewEditor();
+        ActionNewEditor  actionNewEditor  = new ActionNewEditor();
+        ActionOpenEditor actionOpenEditor = new ActionOpenEditor();
+        ActionSaveEditor actionSaveEditor = new ActionSaveEditor();
+        
+        ActionCutText actionCutText  = new ActionCutText();
+        ActionCopyText actionCopyText = new ActionCopyText();
+        ActionPasteText actionPasteText = new ActionPasteText();
         
         MenuBar menuBar = ActionUtils.createMenuBar( Arrays.asList( 
-          new ActionGroup("File",
-              actionNewEditor,
+          new ActionGroup("File", actionNewEditor, actionOpenEditor, actionSaveEditor,
               ActionUtils.ACTION_SEPARATOR,
               new ActionAppExit(primaryStage)),       
-          new ActionGroup("Edit")       
+          new ActionGroup("Edit", actionCutText, actionCopyText, actionPasteText)       
         ));
         
         menuBar.setUseSystemMenuBar(true); // Mac OSX support
         
         
-        ToolBar toolBar = ActionUtils.createToolBar(Arrays.asList(
-           actionNewEditor
-        ));
+        ToolBar toolBar = ActionUtils.createToolBar( Arrays.asList(
+            actionNewEditor, actionOpenEditor, actionSaveEditor,
+            ActionUtils.ACTION_SEPARATOR,
+            actionCutText, actionCopyText, actionPasteText
+        ), ActionTextBehavior.HIDE);
         
         BorderPane content = new BorderPane();
         content.setTop(new VBox(menuBar, toolBar));
@@ -99,15 +106,80 @@ public class Start extends javafx.application.Application implements DocumentEdi
 
         public ActionNewEditor() {
             super("New");
-            setGraphic( new Image("/images/add_file-26.png"));
+            setGraphic(  new Image("/images/file2.png") );
+            setLongText("Create New Document");
         }
 
         @Override public void execute(ActionEvent e) {
             createDocumentEditor();
         }
         
+    }   
+    
+    class ActionOpenEditor extends AbstractAction {
+
+        public ActionOpenEditor() {
+            super("Open");
+            setGraphic(  new Image("/images/folder-open.png") );
+            setLongText("Open Existing Document");
+        }
+
+        @Override public void execute(ActionEvent e) {
+        }
+        
     }    
     
+    class ActionSaveEditor extends AbstractAction {
+
+        public ActionSaveEditor() {
+            super("Save");
+            setGraphic(  new Image("/images/disk.png") );
+            setLongText("Save Document");
+        }
+
+        @Override public void execute(ActionEvent e) {
+        }
+        
+    }   
+    
+    class ActionCutText extends AbstractAction {
+
+        public ActionCutText() {
+            super("Cut");
+            setGraphic(  new Image("/images/scissors.png") );
+            setLongText("Cut");
+        }
+
+        @Override public void execute(ActionEvent e) {
+        }
+        
+    }   
+    
+    class ActionCopyText extends AbstractAction {
+
+        public ActionCopyText() {
+            super("Copy");
+            setGraphic(  new Image("/images/copy.png") );
+            setLongText("Copy");
+        }
+
+        @Override public void execute(ActionEvent e) {
+        }
+        
+    }   
+    
+    class ActionPasteText extends AbstractAction {
+
+        public ActionPasteText() {
+            super("Paste");
+            setGraphic(  new Image("/images/paste2.png") );
+            setLongText("Paste");
+        }
+
+        @Override public void execute(ActionEvent e) {
+        }
+        
+    }       
     static class ActionAppExit extends AbstractAction {
 
         private Stage stage;
